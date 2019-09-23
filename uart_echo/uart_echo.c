2b33b14aec3a99d88175675cfafc561f02291ec9
alishawky <alishawky0007@gmail.com>
2019-09-23 16:06:33 +0200

    Revert "enable switches and develop the estimation logic"
    This reverts commit c55bea3c66a826ac9c67014b8d210cefadaf2752.

--- uart_echo/uart_echo.c
+++ uart_echo/uart_echo.c
@@ -1,6 +1,7 @@
 //*****************************************************************************
 //
-// hello.c - Simple hello world example.
+// uart_echo.c - Example for reading data from and writing data to the UART in
+//               an interrupt driven fashion.
 //
 // Copyright (c) 2012-2017 Texas Instruments Incorporated.  All rights reserved.
 // Software License Agreement
@@ -34,34 +35,27 @@
 #include "driverlib/sysctl.h"
 #include "driverlib/uart.h"
 #include "utils/uartstdio.h"
-
-
 #include "inc/hw_ints.h"
 #include "driverlib/debug.h"
 #include "driverlib/interrupt.h"
 #include "driverlib/timer.h"
-
-
 #include "inc/hw_gpio.h"
-
 #include "inc/hw_timer.h"
 #include "driverlib/timer.h"
-
 #include "inc/hw_uart.h"
-
 #include "inc/hw_ints.h"
 #include "driverlib/interrupt.h"
+
 
 //*****************************************************************************
 //
 //! \addtogroup example_list
-//! <h1>Hello World (hello)</h1>
+//! <h1>UART Echo (uart_echo)</h1>
 //!
-//! A very simple ``hello world'' example.  It simply displays ``Hello World!''
-//! on the UART and is a starting point for more complicated applications.
-//!
-//! UART0, connected to the Virtual Serial Port and running at
-//! 115,200, 8-N-1, is used to display messages from this application.
+//! This example application utilizes the UART to echo text.  The first UART
+//! (connected to the USB debug virtual serial port on the evaluation board)
+//! will be configured in 115,200 baud, 8-n-1 mode.  All characters received on
+//! the UART are transmitted back to the UART.
 //
 //*****************************************************************************
 
@@ -79,16 +73,15 @@ __error__(char *pcFilename, uint32_t ui32Line)
 
 //*****************************************************************************
 //
-// Configure the UART and its pins.  This must be called before UARTprintf().
+// The UART interrupt handler.
 //
 //*****************************************************************************
+
 
 volatile uint32_t  g_ui32Flags;
 volatile uint32_t  seconds;
 volatile int32_t fitch = 0;
 volatile uint32_t  CurrentTime;
-
-
 
 
 void
@@ -110,90 +103,6 @@ Timer0IntHandler(void)
     cOne = HWREGBITW(&g_ui32Flags, 0) ? '1' : '0';
     cTwo = HWREGBITW(&g_ui32Flags, 1) ? '1' : '0';
     ROM_IntMasterEnable();
-}
-
-
-
-void toggle(void)
-{
-    GPIOPinWrite(GPIO_PORTF_BASE, GPIO_PIN_2, GPIO_PIN_2);
-
-    SysCtlDelay(SysCtlClockGet() / 10 / 3);
-
-    GPIOPinWrite(GPIO_PORTF_BASE, GPIO_PIN_2, 0);
-
-    SysCtlDelay(SysCtlClockGet() / 10 / 3);
-}
-
-void
-UART1_Configure(void)
-{
-
-    ROM_SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOB);
-
-    ROM_SysCtlPeripheralEnable(SYSCTL_PERIPH_UART1);
-
-    ROM_GPIOPinConfigure(GPIO_PB0_U1RX);
-    ROM_GPIOPinConfigure(GPIO_PB1_U1TX);
-    ROM_GPIOPinTypeUART(GPIO_PORTB_BASE, GPIO_PIN_0 | GPIO_PIN_1);
-
-    UARTClockSourceSet(UART1_BASE, UART_CLOCK_PIOSC);
-
-    UARTStdioConfig(1, 115200, 16000000);
-}
-
-//*****************************************************************************
-//
-// Print "Hello World!" to the UART on the evaluation board.
-//
-//*****************************************************************************
-
-
-//******************************************************************************************
-void Switch_Enable(void)
-{
-    SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOF);
-    while(!SysCtlPeripheralReady(SYSCTL_PERIPH_GPIOF));
-
-    HWREG(GPIO_PORTF_BASE + GPIO_O_LOCK) = GPIO_LOCK_KEY;
-    HWREG(GPIO_PORTF_BASE + GPIO_O_CR) |= 0x01;
-    HWREG(GPIO_PORTF_BASE + GPIO_O_LOCK) = 0;
-
-    GPIOPinTypeGPIOOutput(GPIO_PORTF_BASE, GPIO_PIN_1 | GPIO_PIN_2 | GPIO_PIN_3);
-    GPIOPinTypeGPIOInput(GPIO_PORTF_BASE, GPIO_PIN_0 | GPIO_PIN_4);
-    GPIOPadConfigSet(GPIO_PORTF_BASE, GPIO_PIN_0 | GPIO_PIN_4, GPIO_STRENGTH_2MA, GPIO_PIN_TYPE_STD_WPU);
-}
-
-void
-UART0_Configure(void)
-{
-
-    ROM_SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOA);
-
-    ROM_SysCtlPeripheralEnable(SYSCTL_PERIPH_UART0);
-
-    ROM_GPIOPinConfigure(GPIO_PA0_U0RX);
-    ROM_GPIOPinConfigure(GPIO_PA1_U0TX);
-    ROM_GPIOPinTypeUART(GPIO_PORTA_BASE, GPIO_PIN_0 | GPIO_PIN_1);
-
-    UARTClockSourceSet(UART0_BASE, UART_CLOCK_PIOSC);
-
-    UARTStdioConfig(0, 115200, 16000000);
-}
-
-
-void GetAndSendInitialTime(void)
-{
-    CurrentTime=  ( UARTCharGet(UART0_BASE) - (48) ) ;
-
-
-
-    CurrentTime = ( (CurrentTime) * (10) ) + ( UARTCharGet(UART0_BASE) -(48) ) ;
-
-
-    UARTCharPut(UART1_BASE,(CurrentTime));
-
-
 }
 
 void Timers_Init(void)
@@ -220,77 +129,85 @@ void Timers_Init(void)
 
 }
 
+void GetAndSendInitialTime(void)
+{
+    CurrentTime=  ( UARTCharGet(UART0_BASE) - (48) ) ;
 
+
+
+    CurrentTime = ( (CurrentTime) * (10) ) + ( UARTCharGet(UART0_BASE) -(48) ) ;
+
+
+    UARTCharPut(UART1_BASE,(CurrentTime));
+
+
+}
+void
+UART1_Configure(void)
+{
+
+    ROM_SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOB);
+
+    ROM_SysCtlPeripheralEnable(SYSCTL_PERIPH_UART1);
+
+    ROM_GPIOPinConfigure(GPIO_PB0_U1RX);
+    ROM_GPIOPinConfigure(GPIO_PB1_U1TX);
+    ROM_GPIOPinTypeUART(GPIO_PORTB_BASE, GPIO_PIN_0 | GPIO_PIN_1);
+
+    UARTClockSourceSet(UART1_BASE, UART_CLOCK_PIOSC);
+
+    UARTStdioConfig(1, 115200, 16000000);
+}
+
+void
+UART0_Configure(void)
+{
+
+    ROM_SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOA);
+
+    ROM_SysCtlPeripheralEnable(SYSCTL_PERIPH_UART0);
+
+    ROM_GPIOPinConfigure(GPIO_PA0_U0RX);
+    ROM_GPIOPinConfigure(GPIO_PA1_U0TX);
+    ROM_GPIOPinTypeUART(GPIO_PORTA_BASE, GPIO_PIN_0 | GPIO_PIN_1);
+
+    UARTClockSourceSet(UART0_BASE, UART_CLOCK_PIOSC);
+
+    UARTStdioConfig(0, 115200, 16000000);
+}
 
 int
 main(void)
 {
 
-
-
-
-    int32_t current_speed = 20;
-    /*INITIALIZE TIMERS*/
-    Timers_Init();
-    /*INITIALIZE INPUT SITCHES*/
-    Switch_Enable();
-    /*INITIALIZE UART0& UART1*/
-    UART1_Configure();
-    UART0_Configure();
-
-    GetAndSendInitialTime();
-
+    ROM_FPUEnable();
     ROM_FPULazyStackingEnable();
 
-    ROM_SysCtlClockSet(SYSCTL_SYSDIV_4 | SYSCTL_USE_PLL | SYSCTL_XTAL_16MHZ |
-                       SYSCTL_OSC_MAIN);
+
+    ROM_SysCtlClockSet(SYSCTL_SYSDIV_1 | SYSCTL_USE_OSC | SYSCTL_OSC_MAIN |
+                       SYSCTL_XTAL_16MHZ);
+
 
     ROM_SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOF);
 
+
     ROM_GPIOPinTypeGPIOOutput(GPIO_PORTF_BASE, GPIO_PIN_2);
+
+
+    ROM_SysCtlPeripheralEnable(SYSCTL_PERIPH_UART0);
+    ROM_SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOA);
+
+    ROM_SysCtlPeripheralEnable(SYSCTL_PERIPH_UART1);
+    ROM_SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOB);
+
+
+    ROM_IntMasterEnable();
+
+
 
 
 
     while(1)
     {
-
-        fitch = UARTCharGet(UART1_BASE);
-
-        if( fitch )
-        {
-            UARTCharPut(UART1_BASE,CurrentTime);
-            UARTCharPut(UART1_BASE,current_speed);
-            toggle();
-
-        }
-
-        if ( GPIOPinRead(GPIO_PORTF_BASE,GPIO_PIN_4) == 0 )
-        {
-            while( GPIOPinRead(GPIO_PORTF_BASE,GPIO_PIN_4) == 0 );
-
-            current_speed++;
-
-            GPIOPinWrite(GPIO_PORTF_BASE, GPIO_PIN_2, GPIO_PIN_2);
-            SysCtlDelay(SysCtlClockGet() / 10 / 3);
-
-            GPIOPinWrite(GPIO_PORTF_BASE, GPIO_PIN_2, 0);
-            SysCtlDelay(SysCtlClockGet() / 10 / 3);
-
-        }
-        else if ( GPIOPinRead(GPIO_PORTF_BASE,GPIO_PIN_0) == 0 )
-        {
-            while( GPIOPinRead(GPIO_PORTF_BASE,GPIO_PIN_0) == 0 );
-
-            current_speed--;
-
-            GPIOPinWrite(GPIO_PORTF_BASE, GPIO_PIN_2, GPIO_PIN_2);
-
-            SysCtlDelay(SysCtlClockGet() / 10 / 3);
-
-            GPIOPinWrite(GPIO_PORTF_BASE, GPIO_PIN_2, 0);
-
-            SysCtlDelay(SysCtlClockGet() / 10 / 3);
-        }
-
     }
 }
