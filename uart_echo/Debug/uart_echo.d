72580abb2c81dd2ab64c06bcb01f7e2aff1a9bd9
alishawky <alishawky0007@gmail.com>
2019-09-23 16:00:17 +0200

    using uart polling

--- uart_echo/Debug/uart_echo.d
+++ uart_echo/Debug/uart_echo.d
@@ -10,16 +10,23 @@ uart_echo.obj: C:/ti/ccs901/ccs/tools/compiler/ti-cgt-arm_18.12.1.LTS/include/ma
 uart_echo.obj: C:/ti/ccs901/ccs/tools/compiler/ti-cgt-arm_18.12.1.LTS/include/machine/_stdint.h
 uart_echo.obj: C:/ti/ccs901/ccs/tools/compiler/ti-cgt-arm_18.12.1.LTS/include/sys/_stdint.h
 uart_echo.obj: C:/ti/ccs901/ccs/tools/compiler/ti-cgt-arm_18.12.1.LTS/include/stdbool.h
-uart_echo.obj: C:/ti/TivaWare_C_Series-2.1.4.178/inc/hw_ints.h
 uart_echo.obj: C:/ti/TivaWare_C_Series-2.1.4.178/inc/hw_memmap.h
+uart_echo.obj: C:/ti/TivaWare_C_Series-2.1.4.178/inc/hw_types.h
 uart_echo.obj: C:/ti/TivaWare_C_Series-2.1.4.178/driverlib/debug.h
 uart_echo.obj: C:/ti/TivaWare_C_Series-2.1.4.178/driverlib/fpu.h
 uart_echo.obj: C:/ti/TivaWare_C_Series-2.1.4.178/driverlib/gpio.h
-uart_echo.obj: C:/ti/TivaWare_C_Series-2.1.4.178/driverlib/interrupt.h
 uart_echo.obj: C:/ti/TivaWare_C_Series-2.1.4.178/driverlib/pin_map.h
 uart_echo.obj: C:/ti/TivaWare_C_Series-2.1.4.178/driverlib/rom.h
+uart_echo.obj: C:/ti/TivaWare_C_Series-2.1.4.178/driverlib/interrupt.h
+uart_echo.obj: C:/ti/TivaWare_C_Series-2.1.4.178/driverlib/timer.h
+uart_echo.obj: C:/ti/TivaWare_C_Series-2.1.4.178/inc/hw_gpio.h
+uart_echo.obj: C:/ti/TivaWare_C_Series-2.1.4.178/inc/hw_timer.h
+uart_echo.obj: C:/ti/TivaWare_C_Series-2.1.4.178/inc/hw_uart.h
+uart_echo.obj: C:/ti/TivaWare_C_Series-2.1.4.178/inc/hw_ints.h
 uart_echo.obj: C:/ti/TivaWare_C_Series-2.1.4.178/driverlib/sysctl.h
 uart_echo.obj: C:/ti/TivaWare_C_Series-2.1.4.178/driverlib/uart.h
+uart_echo.obj: C:/ti/TivaWare_C_Series-2.1.4.178/utils/uartstdio.h
+uart_echo.obj: C:/ti/ccs901/ccs/tools/compiler/ti-cgt-arm_18.12.1.LTS/include/stdarg.h
 
 ../uart_echo.c: 
 C:/ti/ccs901/ccs/tools/compiler/ti-cgt-arm_18.12.1.LTS/include/stdint.h: 
@@ -31,13 +38,20 @@ C:/ti/ccs901/ccs/tools/compiler/ti-cgt-arm_18.12.1.LTS/include/machine/_types.h:
 C:/ti/ccs901/ccs/tools/compiler/ti-cgt-arm_18.12.1.LTS/include/machine/_stdint.h: 
 C:/ti/ccs901/ccs/tools/compiler/ti-cgt-arm_18.12.1.LTS/include/sys/_stdint.h: 
 C:/ti/ccs901/ccs/tools/compiler/ti-cgt-arm_18.12.1.LTS/include/stdbool.h: 
-C:/ti/TivaWare_C_Series-2.1.4.178/inc/hw_ints.h: 
 C:/ti/TivaWare_C_Series-2.1.4.178/inc/hw_memmap.h: 
+C:/ti/TivaWare_C_Series-2.1.4.178/inc/hw_types.h: 
 C:/ti/TivaWare_C_Series-2.1.4.178/driverlib/debug.h: 
 C:/ti/TivaWare_C_Series-2.1.4.178/driverlib/fpu.h: 
 C:/ti/TivaWare_C_Series-2.1.4.178/driverlib/gpio.h: 
-C:/ti/TivaWare_C_Series-2.1.4.178/driverlib/interrupt.h: 
 C:/ti/TivaWare_C_Series-2.1.4.178/driverlib/pin_map.h: 
 C:/ti/TivaWare_C_Series-2.1.4.178/driverlib/rom.h: 
+C:/ti/TivaWare_C_Series-2.1.4.178/driverlib/interrupt.h: 
+C:/ti/TivaWare_C_Series-2.1.4.178/driverlib/timer.h: 
+C:/ti/TivaWare_C_Series-2.1.4.178/inc/hw_gpio.h: 
+C:/ti/TivaWare_C_Series-2.1.4.178/inc/hw_timer.h: 
+C:/ti/TivaWare_C_Series-2.1.4.178/inc/hw_uart.h: 
+C:/ti/TivaWare_C_Series-2.1.4.178/inc/hw_ints.h: 
 C:/ti/TivaWare_C_Series-2.1.4.178/driverlib/sysctl.h: 
 C:/ti/TivaWare_C_Series-2.1.4.178/driverlib/uart.h: 
+C:/ti/TivaWare_C_Series-2.1.4.178/utils/uartstdio.h: 
+C:/ti/ccs901/ccs/tools/compiler/ti-cgt-arm_18.12.1.LTS/include/stdarg.h: 
--- uart_echo/uart_echo.c
+++ uart_echo/uart_echo.c
@@ -25,19 +25,27 @@
 
 #include <stdint.h>
 #include <stdbool.h>
-#include "inc/hw_ints.h"
 #include "inc/hw_memmap.h"
+#include "inc/hw_types.h"
 #include "driverlib/debug.h"
 #include "driverlib/fpu.h"
 #include "driverlib/gpio.h"
-#include "driverlib/interrupt.h"
 #include "driverlib/pin_map.h"
 #include "driverlib/rom.h"
+#include "driverlib/interrupt.h"
+#include "driverlib/timer.h"
+#include "inc/hw_gpio.h"
+#include "inc/hw_timer.h"
+#include "driverlib/timer.h"
+#include "inc/hw_uart.h"
+#include "inc/hw_ints.h"
+#include "driverlib/interrupt.h"
 #include "driverlib/sysctl.h"
 #include "driverlib/uart.h"
+#include "utils/uartstdio.h"
+#include "inc/hw_ints.h"
+#include "driverlib/debug.h"
 
-   uint8_t time[2]= {2,22};
-   uint8_t received_time = 'a';
 
 //*****************************************************************************
 //
@@ -68,174 +76,78 @@ __error__(char *pcFilename, uint32_t ui32Line)
 // The UART interrupt handler.
 //
 //*****************************************************************************
+
+
+volatile uint32_t  g_ui32Flags;
+volatile uint32_t  seconds;
+volatile int32_t fitch = 0;
+volatile uint32_t  CurrentTime;
+
 void
-UARTIntHandler(void)
+UART1_Configure(void)
 {
-    uint32_t ui32Status;
 
-    //
-    // Get the interrrupt status.
-    //
-    ui32Status = ROM_UARTIntStatus(UART0_BASE, true);
+    ROM_SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOB);
 
-    //
-    // Clear the asserted interrupts.
-    //
-    ROM_UARTIntClear(UART0_BASE, ui32Status);
+    ROM_SysCtlPeripheralEnable(SYSCTL_PERIPH_UART1);
 
-    //
-    // Loop while there are characters in the receive FIFO.
-    //
-    while(ROM_UARTCharsAvail(UART0_BASE))
-    {
-        //
-        // Read the next character from the UART and write it back to the UART.
-        //
-        ROM_UARTCharPutNonBlocking(UART0_BASE,  ROM_UARTCharGetNonBlocking(UART0_BASE));
-        received_time = ROM_UARTCharGetNonBlocking(UART0_BASE);
+    ROM_GPIOPinConfigure(GPIO_PB0_U1RX);
+    ROM_GPIOPinConfigure(GPIO_PB1_U1TX);
+    ROM_GPIOPinTypeUART(GPIO_PORTB_BASE, GPIO_PIN_0 | GPIO_PIN_1);
 
-        //
-        // Blink the LED to show a character transfer is occuring.
-        //
-        GPIOPinWrite(GPIO_PORTF_BASE, GPIO_PIN_2, GPIO_PIN_2);
+    UARTClockSourceSet(UART1_BASE, UART_CLOCK_PIOSC);
 
-        //
-        // Delay for 1 millisecond.  Each SysCtlDelay is about 3 clocks.
-        //
-        SysCtlDelay(SysCtlClockGet() / (1000 * 3));
-
-        //
-        // Turn off the LED
-        //
-        GPIOPinWrite(GPIO_PORTF_BASE, GPIO_PIN_2, 0);
-
-    }
+    UARTStdioConfig(1, 115200, 16000000);
 }
 
-void UARTIntHandler1(void)
-{
-    uint32_t ui32Status;
-
-    //
-    // Get the interrrupt status.
-    //
-    ui32Status = ROM_UARTIntStatus(UART1_BASE, true);
-
-    //
-    // Clear the asserted interrupts.
-    //
-    ROM_UARTIntClear(UART1_BASE, ui32Status);
-
-    //
-    // Loop while there are characters in the receive FIFO.
-    //
-    while(ROM_UARTCharsAvail(UART1_BASE))
-    {
-        ROM_UARTCharPutNonBlocking(UART1_BASE,received_time);
-    }
-}
-//*****************************************************************************
-//
-// Send a string to the UART.
-//
-//*****************************************************************************
 void
-UARTSend(const uint8_t *pui8Buffer, uint32_t ui32Count)
+UART0_Configure(void)
 {
-    //
-    // Loop while there are more characters to send.
-    //
-    while(ui32Count--)
-    {
-        //
-        // Write the next character to the UART.
-        //
-        ROM_UARTCharPutNonBlocking(UART0_BASE, *pui8Buffer++);
-    }
+
+    ROM_SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOA);
+
+    ROM_SysCtlPeripheralEnable(SYSCTL_PERIPH_UART0);
+
+    ROM_GPIOPinConfigure(GPIO_PA0_U0RX);
+    ROM_GPIOPinConfigure(GPIO_PA1_U0TX);
+    ROM_GPIOPinTypeUART(GPIO_PORTA_BASE, GPIO_PIN_0 | GPIO_PIN_1);
+
+    UARTClockSourceSet(UART0_BASE, UART_CLOCK_PIOSC);
+
+    UARTStdioConfig(0, 115200, 16000000);
 }
 
-//*****************************************************************************
-//
-// This example demonstrates how to send a string of data to the UART.
-//
-//*****************************************************************************
 int
 main(void)
 {
-    //
-    // Enable lazy stacking for interrupt handlers.  This allows floating-point
-    // instructions to be used within interrupt handlers, but at the expense of
-    // extra stack usage.
-    //
+
     ROM_FPUEnable();
     ROM_FPULazyStackingEnable();
 
-    //
-    // Set the clocking to run directly from the crystal.
-    //
+
     ROM_SysCtlClockSet(SYSCTL_SYSDIV_1 | SYSCTL_USE_OSC | SYSCTL_OSC_MAIN |
                        SYSCTL_XTAL_16MHZ);
 
-    //
-    // Enable the GPIO port that is used for the on-board LED.
-    //
+
     ROM_SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOF);
 
-    //
-    // Enable the GPIO pins for the LED (PF2).
-    //
+
     ROM_GPIOPinTypeGPIOOutput(GPIO_PORTF_BASE, GPIO_PIN_2);
 
-    //
-    // Enable the peripherals used by this example.
-    //
+
     ROM_SysCtlPeripheralEnable(SYSCTL_PERIPH_UART0);
     ROM_SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOA);
 
     ROM_SysCtlPeripheralEnable(SYSCTL_PERIPH_UART1);
     ROM_SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOB);
 
-    //
-    // Enable processor interrupts.
-    //
+
     ROM_IntMasterEnable();
 
-    //
-    // Set GPIO A0 and A1 as UART pins.
-    //
-    GPIOPinConfigure(GPIO_PA0_U0RX);
-    GPIOPinConfigure(GPIO_PA1_U0TX);
-    ROM_GPIOPinTypeUART(GPIO_PORTA_BASE, GPIO_PIN_0 | GPIO_PIN_1);
 
-    GPIOPinConfigure(GPIO_PB0_U1RX);
-    GPIOPinConfigure(GPIO_PB1_U1TX);
-    ROM_GPIOPinTypeUART(GPIO_PORTB_BASE, GPIO_PIN_0 | GPIO_PIN_1);
-    //
-    // Configure the UART for 115,200, 8-N-1 operation.
-    //
-    ROM_UARTConfigSetExpClk(UART0_BASE, ROM_SysCtlClockGet(), 115200,
-                            (UART_CONFIG_WLEN_8 | UART_CONFIG_STOP_ONE |
-                                    UART_CONFIG_PAR_NONE));
-    ROM_UARTConfigSetExpClk(UART1_BASE, ROM_SysCtlClockGet(), 115200,
-                             (UART_CONFIG_WLEN_8 | UART_CONFIG_STOP_ONE |
-                              UART_CONFIG_PAR_NONE));
-    //
-    // Enable the UART interrupt.
-    //
-    ROM_IntEnable(INT_UART0);
-    ROM_UARTIntEnable(UART0_BASE, UART_INT_RX | UART_INT_RT);
 
-    ROM_IntEnable(INT_UART1);
-    ROM_UARTIntEnable(UART1_BASE, UART_INT_RX | UART_INT_RT);
 
-    //
-    // Prompt for text to be entered.
-    //
-    //UARTSend((uint8_t *)"\033[2JEnter text: ", 16);
 
-    //
-    // Loop forever echoing data through the UART.
-    //
     while(1)
     {
     }
